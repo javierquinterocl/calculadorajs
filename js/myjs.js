6b0af4d77e5fb024b2f66255514714c1166c2f30
jaquinterocl <jaquinterocl@ufpso.edu.co>
2023-06-01 02:59:15 -0500

    ajustes enfoque

--- js/myjs.js
+++ js/myjs.js
@@ -2,12 +2,15 @@
 var boton = document.getElementById("boton")
 var fondo = document.getElementById("fondo1")
 var input = document.getElementById("input11");
+var click = document.getSelection("numeroclick")
+
 boton.onclick = function myfuncion(){
    
  
   fondo.classList.toggle("open");
   boton.classList.toggle("open");
   input.classList.toggle("open");
+  click.classList.toggle("open");
     
 
 }
