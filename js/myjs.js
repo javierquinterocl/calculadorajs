7732921ee4270512fd106c132cbea6357e6d3b33
jaquinterocl <jaquinterocl@ufpso.edu.co>
2023-06-01 01:04:01 -0500

    ajustes de tamaño

--- js/myjs.js
+++ js/myjs.js
@@ -1,11 +1,10 @@
 
-var fondoboy = document.querySelector("body");
 var boton = document.getElementById("boton")
 var fondo = document.getElementById("fondo1")
 var input = document.getElementById("input11");
 boton.onclick = function myfuncion(){
    
-  fondoboy.classList.toggle("open");
+ 
   fondo.classList.toggle("open");
   boton.classList.toggle("open");
   input.classList.toggle("open");
